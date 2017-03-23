135b7354ed110be4139cf1b8be0b95c29e2f1c0f
faresBelmadi <fares.belmadi@gmail.com>
2017-03-23 14:13:20 +0100

    Ajout perso + range avec halo

---
+++ Assets/moveRand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRand : MonoBehaviour {
+
+    Behaviour HaloRange;
+	// Use this for initialization
+	void Start () {
+        HaloRange = (Behaviour)GetComponent("Halo");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnMouseDown()
+    {
+        if (HaloRange.enabled)
+            HaloRange.enabled = false;
+        else
+            HaloRange.enabled = true;
+    }
+}
